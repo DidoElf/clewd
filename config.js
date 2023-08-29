b22d1376f6c97fde8f6b8d7448af01ca2d1a3725
DidoElf <139220458+DidoElf@users.noreply.github.com>
2023-08-29 23:35:41 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,90 @@
+/*
+* https://rentry.org/teralomaniac_clewd
+* https://github.com/teralomaniac/clewd
+*/
+
+// SET YOUR COOKIE BELOW
+
+module.exports = {
+    "Cookie": "sessionKey=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",
+    "CookieArray": ["sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",],
+    "Cookiecounter": 30,
+    "Ip": "0.0.0.0",
+    "Port": 8444,
+    "BufferSize": 1,
+    "SystemInterval": 3,
+    "rProxy": "https://claude.ai",
+    "padtxt_placeholder": "",
+    "PromptExperimentFirst": "",
+    "PromptExperimentNext": "",
+    "PersonalityFormat": "{{char}}'s personality: {{personality}}",
+    "ScenarioFormat": "Dialogue scenario: {{scenario}}",
+    "Settings": {
+        "RenewAlways": true,
+        "RetryRegenerate": false,
+        "PromptExperiments": true,
+        "SystemExperiments": true,
+        "PreventImperson": false,
+        "AllSamples": false,
+        "NoSamples": false,
+        "StripAssistant": false,
+        "StripHuman": false,
+        "PassParams": false,
+        "ClearFlags": true,
+        "PreserveChats": true,
+        "LogMessages": true,
+        "FullColon": true,
+        "padtxt": 13500,
+        "xmlPlot": true,
+        "localtunnel": false,
+        "Superfetch": true
+    }
+}
+
+/*
+ BufferSize
+ * How many characters will be buffered before the AI types once
+ * lower = less chance of `PreventImperson` working properly
+
+ ---
+
+ SystemInterval
+ * How many messages until `SystemExperiments alternates`
+
+ ---
+
+ Other settings
+ * https://gitgud.io/ahsk/clewd/#defaults
+ * and
+ * https://gitgud.io/ahsk/clewd/-/blob/master/CHANGELOG.md
+ */
